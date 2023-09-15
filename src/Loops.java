6a1fdc12d48521de401fcc36c1bff7ffb002fb28
alex-l-davidson <alexandra.davidson@sky.uk>
2023-09-15 16:38:35 +0100

    added Loops exercise

---
+++ src/Loops.java
@@ -0,0 +1,76 @@
+public class Loops {
+    public static void main(String[] args) {
+
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println("i = " + i);
+//        }
+//
+//        int i = 0;
+//        while ( i < 10 ) {
+//            System.out.println("i = " + i);
+//            i++;
+//        }
+
+//        do {
+//            System.out.println("i = " + i);
+//            i++;
+//        } while (i < 10 );
+
+//        for (int i = 0, j = 10; i < j; i++, j--) {
+//            System.out.println("i = " + i);
+//            System.out.println("j = " + j);
+//        }
+
+//        int i=0, j=10;
+//        while ( i < j ) {
+//            System.out.println("i = " + i);
+//            System.out.println("j = " + j);
+//            i++;
+//            j--;
+//        }
+
+//        do {
+//            System.out.println("i = " + i);
+//            System.out.println("j = " + j);
+//            i++;
+//            j--;
+//        } while ( i < j );
+
+//        int sum = 0;
+//        for (int i = 0; i < 10; sum += i, i++) {
+//            System.out.println("i = " + i + ", sum = " + sum);
+//        }
+
+//        int sum = 0, i = 0;
+//        while ( i < 10 ) {
+//            System.out.println("i = " + i + ", sum = " + sum);
+//            sum += i;
+//            i ++;
+//        }
+
+//        do {
+//            System.out.println("i = " + i + ", sum = " + sum);
+//            sum += i;
+//            i ++;
+//        } while ( i < 10);
+
+//        int x = 10;
+//        while (x > 0) //there was a semi-colon here
+//        {
+//            System.out.println(x--);
+//        }
+//        System.out.println("We have lift off!");
+
+//        int x = 10;
+//        while (x > 0) {
+//            System.out.println("x is " + x);
+//            x--; //it was infinite as the counter wasn't incremented
+//        }
+
+//        int sum = 0, i = 0; //i needed to be initialised
+//            for (; i < 10; sum += i++) { //put a print statement in the loop so we could see
+//                System.out.println("Sum is " + sum);
+//            }
+//        System.out.println("Sum is " + sum);
+    }
+}
