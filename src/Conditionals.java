b697e52b49f02d8e5aa26c1a4d53247247411b59
alex-l-davidson <alexandra.davidson@sky.uk>
2023-09-15 14:14:00 +0100

    adding conditionals exercise work

---
+++ src/Conditionals.java
@@ -0,0 +1,26 @@
+public class Conditionals {
+    public static void main(String[] args) {
+//        int x=3, y=5;
+//        if (x >= 0) {
+//            if (y < x) {
+//                System.out.println("y is less than x");
+//            } else {
+//                System.out.println("y is greater than x");
+//            }
+//        } else {
+//            System.out.println("x is negative");
+//        }
+
+//        int x = 7;
+//        if (x == 0) {
+//            System.out.println("x is zero");
+//        }
+
+        int x = 15, y = 24;
+        if ( x % 2 == 0 && y % 2 == 0 ) {
+            System.out.println("x and y are even numbers");
+        } else {
+            System.out.println("x and y are not both even");
+        }
+    }
+}
